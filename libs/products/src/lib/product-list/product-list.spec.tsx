a34e69bfd9dc1e9298f88ed58a5fb3fa1a756aa3
sarveshhome <sarvesh.carlton@gmail.com>
2023-10-28 15:48:46 +0530

    unit test cases

--- libs/products/src/lib/product-list/product-list.spec.tsx
+++ libs/products/src/lib/product-list/product-list.spec.tsx
@@ -1,10 +1,27 @@
 import { render } from '@testing-library/react';
-
-import ProductList from './product-list';
+import React from 'react';
+import ProductList, { ProductListProps } from './product-list';
 
 describe('ProductList', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<ProductList />);
     expect(baseElement).toBeTruthy();
   });
+});
+
+describe('ProductList Component', () => {
+  
+  it('displays the welcome message', () => {
+    const props: ProductListProps = {};
+    const { getByText } = render(<ProductList {...props} />);
+    expect(getByText('Welcome to ProductList!')).toBeTruthy();
+  });
+
+  it('displays the secondary message', () => {
+    const props: ProductListProps = {};
+    const { getByText } = render(<ProductList {...props} />);
+    expect(getByText('Good')).toBeTruthy();
+  });
+
+  // You can add more test cases as needed for specific functionality or edge cases
 });
--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -11,4 +11,6 @@ export function Products(props: ProductsProps) {
   );
 }
 
+
+
 export default Products;
